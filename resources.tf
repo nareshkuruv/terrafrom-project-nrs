45dede09d779cf09028f4778a7e19884300b5ff5
nareshkuruv <118480501+nareshkuruv@users.noreply.github.com>
2023-01-04 16:53:46 +0530

    updated

--- resources.tf
+++ resources.tf
@@ -1,6 +1,7 @@
-resource "aws_instance" "jenkins-server-ter" {
+resource "aws_instance" "mycloudserver" {
     ami = "ami-0cca134ec43cf708f"
     instance_type = "t2.micro"
     tags = {
-    Name = "jenkins-server-ter"
+    Name = "mycloudserver"
    }
+}
